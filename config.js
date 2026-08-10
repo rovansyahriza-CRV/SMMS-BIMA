898efb437c62493b6ec521bb343d55c3ca1aba4a
rovansyahriza-CRV <rovan.syahriza@gmail.com>
2026-08-10 14:29:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // URL Web App Apps Script — SMMS-BIMA
 const RESOURCES_URL = "https://script.google.com/macros/s/AKfycbzHvD3cz2Ur3WGkf_b-yIC6qNRajqZC9mx5ZmcQzr1-28vx_7EJO12Paez5VZFQt3IO/exec";
 const TRANSAKSI_URL = "https://script.google.com/macros/s/AKfycbziA0AWUvyt8f6VGyy4mWM_nsv7RP2xjfrK4vVzPR3_65Qdiu8_NPn8KZyYuPQ6USRW/exec";
-const KARYAWAN_URL = "https://script.google.com/macros/s/AKfycbyCZxGfKdCtCA1O8SzdJuxPPM-UVpWtk3QXQJpLZEBXFPGYPnGbq36b5WBDEruzr95iiQ/exec"; 
+const KARYAWAN_URL = "https://script.google.com/macros/s/AKfycbxDIDJ5COlR2GOCcmJdN0_j303qxCpk_mKUFzzOytgzmgPsV244_vWgS-XUaYxmSJ_CTg/exec";
 
 // Grup yang butuh input durasi sewa (bukan konsumsi habis pakai)
 const RENTAL_GROUPS = ["Tools", "HeavyEquipment"];
