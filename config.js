35b0ac5c0724c87dcb23918813ca42cc49f2f373
rovansyahriza-CRV <rovan.syahriza@gmail.com>
2026-08-25 11:36:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // URL Web App Apps Script — SMMS-BIMA
-const RESOURCES_URL = "https://script.google.com/macros/s/AKfycbzHvD3cz2Ur3WGkf_b-yIC6qNRajqZC9mx5ZmcQzr1-28vx_7EJO12Paez5VZFQt3IO/exec";
+const RESOURCES_URL = "https://script.google.com/macros/s/AKfycby1cpMck3yRYjyq1qtjNrMUeKQgZlVvVkB9PTwBHuy35QahNBuBdBzRiYm9E9psoebX/exec";
 const TRANSAKSI_URL = "https://script.google.com/macros/s/AKfycbziA0AWUvyt8f6VGyy4mWM_nsv7RP2xjfrK4vVzPR3_65Qdiu8_NPn8KZyYuPQ6USRW/exec";
 const KARYAWAN_URL = "https://script.google.com/macros/s/AKfycbxDIDJ5COlR2GOCcmJdN0_j303qxCpk_mKUFzzOytgzmgPsV244_vWgS-XUaYxmSJ_CTg/exec";
 
